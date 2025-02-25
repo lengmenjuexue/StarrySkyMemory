49858669715639c7164fbc3ccdab0f2a5c507e35
lengmenjuexue <69681346+lengmenjuexue@users.noreply.github.com>
2025-02-25 20:37:14 +0800

    Add files via upload

---
+++ 星空记纪相关本体的rdflib解析辅助类/CPR.py
@@ -0,0 +1,29 @@
+from rdflib.namespace import DefinedNamespace, Namespace
+from rdflib.term import URIRef
+
+class CPR(DefinedNamespace):
+    _NS = Namespace("http://purl/lmjxt/cpr/core#")
+    op_JiShiFangShi : URIRef
+    op_NeiRongLaiYuan : URIRef
+    op_TeZhengMiaoShu : URIRef
+    op_TianXiangLeiXing : URIRef
+    op_WenXianLaiYuan : URIRef
+    op_XiLiRiQi : URIRef
+    op_ZhongLiRiQi : URIRef
+    dp_Juan : URIRef
+    dp_RecordID : URIRef
+    dp_TeZhengMiaoShu_text : URIRef
+    dp_WenXianMingCheng : URIRef
+    dp_XiLiRiQi_text : URIRef
+    dp_XinXiLaiYuan : URIRef
+    dp_YeMa : URIRef
+    dp_YuanShiWenBen : URIRef
+    dp_ZhongLiRiQi_text : URIRef
+    c_JiShiFangShi : URIRef
+    c_NeiRongLaiYuan : URIRef
+    c_TianXiang : URIRef
+    c_TianXiangJiLu : URIRef
+    c_TianXiangTeZhengMiaoShu : URIRef
+
+
+
